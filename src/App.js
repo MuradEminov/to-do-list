239ec250e1ba142a3e246850c83ccc25c0e2d69d
MuradEminov <murademinov88@gmail.com>
2021-04-16 16:50:40 +0400

    Add items to the list done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({ text: '', key: '' });
+
+  const handleInput = (e) => {
+    setCurrentItem({ ...currentItem, text: e.target.value, key: Date.now() });
+    // console.log(currentItem.text);
+    // console.log(currentItem.key);
+  };
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = currentItem;
+    if (newItem.text !== '') {
+      setItems([...items, currentItem]);
+      console.log('Current items: ', items);
+    }
+    setCurrentItem({ text: '', key: '' });
+  };
+
   return (
     <div>
       <div className='App'>
         <header>
-          <form id='to-do-form'>
-            <input type='text' placeholder='Bitte geben den Text' />
+          <form id='to-do-form' onSubmit={(e) => addItem(e)}>
+            <input
+              type='text'
+              placeholder='Bitte geben den Text'
+              value={currentItem.text}
+              onChange={(e) => handleInput(e)}
+            />
             <button type='add'>hinzufügen</button>
           </form>
         </header>
