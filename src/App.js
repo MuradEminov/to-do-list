00cb482faa536e2ab318b22e4a60d1e91960cf1d
MuradEminov <murademinov88@gmail.com>
2021-04-16 13:49:41 +0400

    Basic form is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='App'>
+        <header>
+          <form id='to-do-form'>
+            <input type='text' placeholder='Bitte geben den Text' />
+            <button type='add'>hinzufügen</button>
+          </form>
+        </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
