5d9399e7c707aa189391a5c32cccb1280e977a9c
MuradEminov <murademinov88@gmail.com>
2021-04-18 17:48:18 +0400

    Display done items number added

--- src/DoneItems.js
+++ src/DoneItems.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './DoneItems.css';
 import DoneListItems from './DoneListItems';
+import { DoneListContext } from './App';
 
 const DoneItems = (props) => {
+  const doneList = useContext(DoneListContext);
   return (
     <div className='Done'>
       <DoneListItems />
+      <p id='done-items'>Einträge bereits abgehakt: {doneList.length} </p>
     </div>
   );
 };
